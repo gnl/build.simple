fe97abf1553877880841e2afba102da4b63e31c4
George Lipov <gnl@lipov.net>
2023-10-11 17:36:37 +0200

    Initial commit
    Signed-off-by: George Lipov <gnl@lipov.net>

---
+++ src/build/simple/core.clj
@@ -0,0 +1,118 @@
+;; Copyright (c) George Lipov. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 2.0 (https://choosealicense.com/licenses/epl-2.0/) which can
+;; be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+(ns ^:no-doc build.simple.core
+  "
+  1. Update project.edn
+  2. `clj -T:build install`
+  3. `git commit --all --gpg-sign`
+  4. `git tag --sign <v0.1.0>`
+  5. `clj -T:build deploy` (optional: `:sign false`, `:check false`)
+  "
+  (:require
+   [clojure.tools.build.api :as b]
+   [build.simple.utils
+    :refer [step done maybe-load-config! validate-version snapshot-version?
+            distribution-repos predeploy-checks read-credentials]]))
+
+
+(def !config (atom nil))
+
+
+(defn clean [_]
+  (maybe-load-config! !config)
+  (step "Cleaning build")
+  (doseq [dir (:clean-dirs @!config)]
+    (b/delete {:path dir}))
+  (done))
+
+
+(defn pom [_]
+  (maybe-load-config! !config)
+  (validate-version (:version @!config))
+  (step "Generating pom.xml")
+  (b/write-pom (select-keys @!config
+                            [:basis
+                             :class-dir
+                             :lib
+                             :version
+                             :scm
+                             :src-dirs
+                             :resource-dirs
+                             :repos
+                             :pom-data]))
+  (done))
+
+
+(defn jar [_]
+  (maybe-load-config! !config)
+  (clean nil)
+  (pom nil)
+  (step "Building jar")
+  (b/copy-dir {:src-dirs   (mapcat val
+                                   (select-keys @!config [:src-dirs :resource-dirs]))
+               :target-dir (:class-dir @!config)})
+  (b/jar (select-keys @!config [:class-dir :jar-file]))
+  (done))
+
+
+(defn install [_]
+  (maybe-load-config! !config)
+  (jar nil)
+  (step "Installing jar to local repo")
+  (b/install (select-keys @!config [:basis :lib :version :class-dir :jar-file]))
+  (done))
+
+
+(defn deploy
+  "Securely handles the Clojars credentials by prompting the user and passing
+  them on to Maven via the environment. gpg-agent should be set up correctly as
+  well (incl. a graphical pinentry interface) so that the signing key passphrase
+  doesn't have to be cached, stored or provided to Maven in an insecure manner
+  (or at all)."
+  [{:keys [sign check]}]
+  (assert (or (nil? sign) (boolean? sign))
+          "`:sign` has to be `true` or `false`")
+  (assert (or (nil? check) (boolean? check))
+          "`:check` has to be `true` or `false`")
+  (maybe-load-config! !config)
+  (let [{:keys [lib version signing-key class-dir jar-file]} @!config
+        snapshot? (snapshot-version? version)
+        sign?     (and signing-key
+                       (not (false? sign))
+                       (not snapshot?))
+        repos     (distribution-repos @!config)]
+    (assert (not-empty repos)
+            "Distribution repositories not found in project.edn – add `[:distributionManagement [:repository ...]]` element to `:pom-data` hiccup.")
+    (when (and (not snapshot?)
+               (not (false? check)))
+      (predeploy-checks @!config))
+    (jar nil)
+    (doseq [{:keys [id url]} repos]
+      (step (format "\nDeploying %s jar to `%s` repo"
+                    (if sign? "signed" "unsigned")
+                    id))
+      (b/process {:env (read-credentials id)
+                  :command-args
+                  (remove nil?
+                          ["mvn"
+                           (if sign?
+                             "gpg:sign-and-deploy-file"
+                             "deploy:deploy-file")
+                           (str "-Dfile=" jar-file)
+                           (format "-DpomFile=%s/META-INF/maven/%s/pom.xml"
+                                   class-dir
+                                   lib)
+                           (str "-DrepositoryId=" id)
+                           (str "-Durl=" url)
+                           (when sign?
+                             (str "-Dgpg.keyname=" signing-key))])}))
+    (when sign?
+      (println "\n")
+      (doseq [hash-algo ["sha512" "rmd160"]]
+        (b/process {:command-args ["openssl" hash-algo jar-file]})))))
---
+++ src/build/simple/utils.clj
@@ -0,0 +1,117 @@
+;; Copyright (c) George Lipov. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 2.0 (https://choosealicense.com/licenses/epl-2.0/) which can
+;; be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+(ns build.simple.utils
+  (:require [clojure.edn :as edn]
+            [clojure.string :as string]
+            [clojure.tools.build.api :as b]))
+
+
+(def version-tag-regex #"^v(\d+\.\d+\.\d+)(-\w+)?$")
+
+
+(defn snapshot-version?
+  [version]
+  (string/ends-with? version "-SNAPSHOT"))
+
+
+(defn step [s]
+  (print (str s "... "))
+  (flush))
+
+
+(defn done []
+  (println "DONE"))
+
+
+(defn validate-version [version]
+  (step (format "Validating version %s" version))
+  (assert (re-matches version-tag-regex (str "v" version))
+          "Invalid version format – should look like 0.0.0 or 0.0.0-qualifier.")
+  (done))
+
+
+(defn predeploy-checks [config]
+  (step "Performing predeploy checks")
+  (assert (string/blank? (b/git-process {:git-args ["status" "--porcelain"]}))
+          "Git working tree is not clean.")
+  (let [git-tag (b/git-process {:git-args ["describe" "--tags"]})]
+    (assert (not (string/blank? git-tag))
+            "Missing Git version tag.")
+    (assert (re-matches version-tag-regex git-tag)
+            "Invalid Git tag – should look like v0.0.0 or v0.0.0-qualifier.")
+    (assert (= (str "v" (:version config)) git-tag)
+            "Git tag doesn't match version in project.edn."))
+  (done))
+
+
+(defn read-credentials [repo-id]
+  (println "\n")
+  (let [console (System/console)
+        user    (.readLine console (format "Username for `%s` repo: " repo-id) nil)
+        ;; REVIEW: This prints the prompt twice for some reason.
+        token   (apply str (.readPassword console "Deploy password or token: " nil))]
+    ;; Make sure the username and password vars in ~/.m2/settings.xml are set
+    ;; correctly for each repository ID, e.g:
+    ;<server>
+    ;  <id>clojars</id>
+    ;  <username>${env.clojars_username}</username>
+    ;  <password>${env.clojars_password}</password>
+    ;</server>
+    (println "...")
+    {(str repo-id "_username") user
+     (str repo-id "_password") token}))
+
+
+(defn distribution-repos
+  [{:keys [pom-data] :as _config}]
+  (let [repos (some->> pom-data
+                       (drop-while #(not= (first %) :distributionManagement))
+                       first
+                       rest
+                       (filter #(= (first %) :repository))
+                       (map rest))]
+    (for [repo-entries repos]
+      (into {} repo-entries))))
+
+
+(defn maybe-load-config!
+  [*config]
+  (when (nil? @*config)
+    (let [deps-config    (edn/read-string (slurp "deps.edn"))
+          project-config (edn/read-string (slurp "project.edn"))
+          scm-url-root   (some->> project-config
+                                  :scm
+                                  :url
+                                  (re-matches #"^https?:/+(.+?)/*$")
+                                  second)
+          default-scm    (when scm-url-root
+                           {:connection
+                            (format "scm:git:git://%s.git" scm-url-root)
+                            :developerConnection
+                            (format "scm:git:ssh://git@%s.git" scm-url-root)})
+          version        (:version project-config)
+          lib            (:lib project-config)
+          ;; NOTE: Automatically setting Clojars as the default deployment
+          ;; target here – when none are set explicitly – was briefly considered
+          ;; and ultimately rejected due to the risk of accidentally dumping a
+          ;; closed-source project onto the internets.
+          full-config    (merge {:src-dirs   (:paths deps-config)
+                                 :clean-dirs ["target"]}
+                                project-config
+                                {:scm (merge default-scm
+                                             (:scm project-config)
+                                             (when-not (snapshot-version? version)
+                                               {:tag (str "v" version)}))}
+                                {:deps      deps-config
+                                 :basis     (b/create-basis (select-keys project-config [:aliases]))
+                                 :class-dir "target/classes"
+                                 :jar-file  (format "target/%s-%s.jar"
+                                                    (name lib)
+                                                    version)})]
+      (reset! *config full-config))))
